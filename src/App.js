1f944c498354aca18c066cdbf6a65af5d7482721
Shreya Rajput <15ucs134@lnmiit.ac.in>
2020-04-26 07:06:44 -0700

    button created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
+import Notes from "./components/Notes";
+
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteText: "",
+      notes: []
+    };
+  }
+
+  updateNoteText(noteText) {
+    this.setState({ noteText: noteText.target.value });
+  }
+
+  handleKeyPress = event => {
+    if (event.key === "Enter") {
+      let notesArr = this.state.notes;
+      notesArr.push(this.state.noteText);
+      this.setState({ noteText: "" });
+    }
+  };
+
+  deleteNote(index) {
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1);
+    this.setState({ notes: notesArr });
+  }
+
+  addNote() {
+    if (this.state.noteText === "") {
+      return;
+    }
+
+    let notesArr = this.state.notes;
+    notesArr.push(this.state.noteText);
+    this.setState({ noteText: "" });
+    this.textInput.focus();
+  }
+
+  render() {
+    let notes = this.state.notes.map((val, key) => {
+      return (
+        <Notes key={key} text={val} deleteMethod={() => this.deleteNote(key)} />
+      );
+    });
+
+    return (
+      <div className=" Container">
+        <div className="header">Rreact Todo Application</div>
+        {notes}
+        <div className="btn" onClick={this.addNote.bind(this)}>
+          +
+        </div>
+        <input
+          type="text"
+          ref={input => {
+            this.textInput = input;
+          }}
+          value={this.state.noteText}
+          onChange={noteText => this.updateNoteText(noteText)}
+          onKeyPress={this.handleKeyPress.bind(this)}
+        />
+      </div>
+    );
+  }
 }
+
+export default App;
